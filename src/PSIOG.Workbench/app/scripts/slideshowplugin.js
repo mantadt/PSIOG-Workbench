4213d9bb46fe1de8943cc0fb62d57121fc176e5b
Aditya Manthena <aditya.manthena@psiog.com>
2017-06-14 20:43:19 +0530

    partial commit - slideshow plugin

---
+++ src/PSIOG.Workbench/app/scripts/slideshowplugin.js
@@ -0,0 +1,39 @@
+﻿angular.module('sbAdminApp').service('slideshowService', function ($http, $q) {
+    this.getSlideshowJSON = function (flowchart, flow) {
+
+        var one = $http({
+            method: 'GET',
+            url: 'http://192.168.10.132:1337/getFlowChartByID/' + flowchart,
+            data: '',
+            headers: {
+                'Content-Type': 'application/json; charset=utf-8'
+            }
+        });
+
+        var two = $http({
+            method: 'GET',
+            url: 'http://192.168.10.132:1337/getCoordbyFlowId/' + flowchart,
+            data: '',
+            headers: {
+                'Content-Type': 'application/json; charset=utf-8'
+            }
+        });
+
+        $q.all([one, two]).then(function success(ret) {
+            var json = ret[0].data;
+            var coords = ret[1].data;
+            var blocks = flow.split('_');
+            var result = { coOrdinates: [] };
+
+            for (var iL = 0; iL < blocks.length; iL++) {
+                var file = { fileId: "", coOrds: [] };
+
+                result.push(file);
+            } 
+
+            return result;
+        }, function error(ret) {
+            Console.log('error');
+        });
+    };
+});

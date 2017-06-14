4eed7f42907f05cfd053a153d9390f45492e0448
Sudharshan Ashok <sudharshanpsiog@outlook.com>
2017-06-14 16:13:58 +0530

    testflow commit

--- src/PSIOG.Workbench/app/scripts/app.js
+++ src/PSIOG.Workbench/app/scripts/app.js
@@ -8,134 +8,160 @@
  * Main module of the application.
  */
 var app = angular
-    .module('sbAdminApp', [
-        'oc.lazyLoad',
-        'ui.router',
-        'ui.bootstrap',
-        'angular-loading-bar',
-    ])
-    .config(['$stateProvider', '$urlRouterProvider', '$ocLazyLoadProvider', function ($stateProvider, $urlRouterProvider, $ocLazyLoadProvider) {
+  .module('sbAdminApp', [
+    'oc.lazyLoad',
+    'ui.router',
+    'ui.bootstrap',
+    'angular-loading-bar',
+  ])
+  .config(['$stateProvider','$urlRouterProvider','$ocLazyLoadProvider',function ($stateProvider,$urlRouterProvider,$ocLazyLoadProvider) {
+    
+    $ocLazyLoadProvider.config({
+      debug:false,
+      events:true,
+    });
 
-        $ocLazyLoadProvider.config({
-            debug: false,
-            events: true,
-        });
+    $urlRouterProvider.otherwise('/dashboard/home');
 
-        $urlRouterProvider.otherwise('/dashboard/CreateFlowChart');
-
-        $stateProvider
-            .state('dashboard', {
-                url: '/dashboard',
-                templateUrl: 'views/dashboard/main.html',
-                resolve: {
-                    loadMyDirectives: function ($ocLazyLoad) {
-                        return $ocLazyLoad.load(
-                            {
-                                name: 'sbAdminApp',
-                                files: [
-                                    'scripts/directives/header/header.js',
-                                    'scripts/directives/header/header-notification/header-notification.js',
-                                    'scripts/directives/sidebar/sidebar.js',
-                                    'scripts/directives/sidebar/sidebar-search/sidebar-search.js',
-                                    'scripts/directives/flowchartdd.js',
-                                    'scripts/directives/generator/generator.js'
-                                ]
-                            }),
-                            $ocLazyLoad.load(
-                                {
-                                    name: 'toggle-switch',
-                                    files: ["bower_components/angular-toggle-switch/angular-toggle-switch.min.js",
-                                        "bower_components/angular-toggle-switch/angular-toggle-switch.css"
-                                    ]
-                                }),
-                            $ocLazyLoad.load(
-                                {
-                                    name: 'ngAnimate',
-                                    files: ['bower_components/angular-animate/angular-animate.js']
-                                }),
-                        $ocLazyLoad.load(
-                            {
-                                name: 'ngCookies',
-                                files: ['bower_components/angular-cookies/angular-cookies.js']
-                            }),
-                        $ocLazyLoad.load(
-                            {
-                                name: 'ngResource',
-                                files: ['bower_components/angular-resource/angular-resource.js']
-                            }),
-                        $ocLazyLoad.load(
-                            {
-                                name: 'ngSanitize',
-                                files: ['bower_components/angular-sanitize/angular-sanitize.js']
-                            }),
-                        $ocLazyLoad.load(
-                            {
-                                name: 'ngTouch',
-                                files: ['bower_components/angular-touch/angular-touch.js']
-                            })
-                    }
-                }
+    $stateProvider
+      .state('dashboard', {
+        url:'/dashboard',
+        templateUrl: 'views/dashboard/main.html',
+        resolve: {
+            loadMyDirectives:function($ocLazyLoad){
+                return $ocLazyLoad.load(
+                {
+                    name:'sbAdminApp',
+                    files:[
+                    'scripts/directives/header/header.js',
+                    'scripts/directives/header/header-notification/header-notification.js',
+                    'scripts/directives/sidebar/sidebar.js',
+                    'scripts/directives/sidebar/sidebar-search/sidebar-search.js',
+                    'scripts/directives/flowchartdd.js',
+                    'scripts/directives/generator/generator.js'
+                    ]
+                }),
+                $ocLazyLoad.load(
+                {
+                   name:'toggle-switch',
+                   files:["bower_components/angular-toggle-switch/angular-toggle-switch.min.js",
+                          "bower_components/angular-toggle-switch/angular-toggle-switch.css"
+                      ]
+                }),
+                $ocLazyLoad.load(
+                {
+                  name:'ngAnimate',
+                  files:['bower_components/angular-animate/angular-animate.js']
+                })
+                // $ocLazyLoad.load(
+                // {
+                //   name:'ngCookies',
+                //   files:['bower_components/angular-cookies/angular-cookies.js']
+                // })
+                // $ocLazyLoad.load(
+                // {
+                //   name:'ngResource',
+                //   files:['bower_components/angular-resource/angular-resource.js']
+                // })
+                // $ocLazyLoad.load(
+                // {
+                //   name:'ngSanitize',
+                //   files:['bower_components/angular-sanitize/angular-sanitize.js']
+                // })
+                // $ocLazyLoad.load(
+                // {
+                //   name:'ngTouch',
+                //   files:['bower_components/angular-touch/angular-touch.js']
+                // })
+            }
+        }
+    })
+      .state('dashboard.home',{
+        url:'/home',
+        controller: 'MainCtrl',
+        templateUrl:'views/dashboard/home.html',
+        resolve: {
+          loadMyFiles:function($ocLazyLoad) {
+            return $ocLazyLoad.load({
+              name:'sbAdminApp',
+              files:[
+              'scripts/controllers/main.js',
+              'scripts/directives/timeline/timeline.js',
+                  'scripts/directives/notifications/notifications.js',
+              'scripts/directives/generator/generator.js',
+              'scripts/directives/chat/chat.js',
+              'scripts/directives/dashboard/stats/stats.js'
+              ]
             })
-            .state('dashboard.home', {
-                url: '/home',
-                controller: 'MainCtrl',
-                templateUrl: 'views/dashboard/home.html',
-                resolve: {
-                    loadMyFiles: function ($ocLazyLoad) {
-                        return $ocLazyLoad.load({
-                            name: 'sbAdminApp',
-                            files: [
-                                'scripts/controllers/main.js',
-                                'scripts/directives/timeline/timeline.js',
-                                'scripts/directives/notifications/notifications.js',
-                                'scripts/directives/chat/chat.js',
-                                'scripts/directives/dashboard/stats/stats.js'
-                            ]
-                        })
-                    }
-                }
+          }
+        }
+      })
+      .state('dashboard.form',{
+        templateUrl:'views/form.html',
+        url:'/form'
+    })
+      .state('dashboard.blank',{
+        templateUrl:'views/pages/blank.html',
+        url:'/blank'
+    })
+      .state('login',{
+        templateUrl:'views/pages/login.html',
+        url:'/login'
+    })
+      .state('dashboard.chart',{
+        templateUrl:'views/chart.html',
+        url:'/chart',
+        controller:'ChartCtrl',
+        resolve: {
+          loadMyFile:function($ocLazyLoad) {
+            return $ocLazyLoad.load({
+              name:'chart.js',
+              files:[
+                'bower_components/angular-chart.js/dist/angular-chart.min.js',
+                'bower_components/angular-chart.js/dist/angular-chart.css'
+              ]
+            }),
+            $ocLazyLoad.load({
+                name:'sbAdminApp',
+                files:['scripts/controllers/chartContoller.js']
             })
-            .state('dashboard.form', {
-                templateUrl: 'views/form.html',
-                url: '/form'
-            })
-            .state('dashboard.blank', {
-                templateUrl: 'views/pages/blank.html',
-                url: '/blank'
-            })
-            .state('login', {
-                templateUrl: 'views/pages/login.html',
-                url: '/login'
-            })
-            .state('dashboard.chart', {
-                templateUrl: 'views/chart.html',
-                url: '/chart',
-                controller: 'ChartCtrl',
-                resolve: {
-                    loadMyFile: function ($ocLazyLoad) {
-                        return $ocLazyLoad.load({
-                            name: 'chart.js',
-                            files: [
-                                'bower_components/angular-chart.js/dist/angular-chart.min.js',
-                                'bower_components/angular-chart.js/dist/angular-chart.css'
-                            ]
-                        }),
-                            $ocLazyLoad.load({
-                                name: 'sbAdminApp',
-                                files: ['scripts/controllers/chartContoller.js']
-                            })
-                    }
-                }
-            })
-            .state('dashboard.table', {
-                templateUrl: 'views/table.html',
-                url: '/table'
-            })
-            .state('dashboard.panels-wells', {
-                templateUrl: 'views/ui-elements/panels-wells.html',
-                url: '/panels-wells'
-            })
-            .state('dashboard.CreateFlowChart', {
+          }
+        }
+    })
+      .state('dashboard.table',{
+        templateUrl:'views/table.html',
+        url:'/table'
+    })
+      .state('dashboard.panels-wells',{
+          templateUrl:'views/ui-elements/panels-wells.html',
+          url:'/panels-wells'
+        })
+       
+      .state('dashboard.buttons',{
+        templateUrl:'views/ui-elements/buttons.html',
+        url:'/buttons'
+    })
+      .state('dashboard.notifications',{
+        templateUrl:'views/ui-elements/notifications.html',
+        url:'/notifications'
+    })
+      .state('dashboard.typography',{
+       templateUrl:'views/ui-elements/typography.html',
+       url:'/typography'
+   })
+      .state('dashboard.icons',{
+       templateUrl:'views/ui-elements/icons.html',
+       url:'/icons'
+   })
+      .state('dashboard.grid',{
+       templateUrl:'views/ui-elements/grid.html',
+       url:'/grid'
+        })
+      .state('dashboard.generateTests', {
+       templateUrl: 'views/pages/generateTests.html',
+       url: '/generateTests'
+  })
+.state('dashboard.CreateFlowChart', {
                 templateUrl: 'views/ui-elements/CreateFlowChart.html',
                 url: '/CreateFlowChart'
             })
@@ -155,43 +181,15 @@ var app = angular
                 templateUrl: 'views/ui-elements/ViewPresentations.html',
                 url: '/CreateUsability'
             })
-            .state('dashboard.buttons', {
-                templateUrl: 'views/ui-elements/buttons.html',
-                url: '/buttons'
-            })
-            .state('dashboard.notifications', {
-                templateUrl: 'views/ui-elements/notifications.html',
-                url: '/notifications'
-            })
-            .state('dashboard.typography', {
-                templateUrl: 'views/ui-elements/typography.html',
-                url: '/typography'
-            })
-            .state('dashboard.icons', {
-                templateUrl: 'views/ui-elements/icons.html',
-                url: '/icons'
-            })
-            .state('dashboard.grid', {
-                templateUrl: 'views/ui-elements/grid.html',
-                url: '/grid'
-            })
-            .state('dashboard.generateTests', {
-              templateUrl: 'views/pages/generateTests.html',
-              url: '/generateTests'
-             })
+            
+          
     }]);
 
-angular.module('sbAdminApp')
-    .controller('sampleController', ['$scope', function($scope){
-        $scope.toggleJsonXml = function(){
-            alert('abcd');
-        }
-    }])
 
-angular
-    .module('sbAdminApp').factory('generatorService', ['$http', function ($http) {
+//Service to Generate all combinations of flows
+app.factory('generatorService', ['$http', function ($http) {
     var data = { name: 'MS' };
-    var urlBase = 'http://localhost:3000/';
+    var urlBase = 'http://192.168.10.132:1337/';
     return {
         generateFromXml: function (xmlData) {
             var formData = new FormData();
@@ -201,13 +199,16 @@ angular
         },
 
         generateFromJson: function (jsonData) {
+            //var formData = new FormData();
+            //formData.append("flowChartID", id);
             return $http.post(urlBase + "generateFromJson/", jsonData, { headers: { 'Content-Type': 'application/json' } });
         },
 
-        getCall: function () {
-            return $http.get(urlBase)
+        getJsonValue: function(id){
+          return $http.get(urlBase+"getFlowChartByID/"+id, { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } });
         }
-    }
-}]
 
-);
+      
+    }
+}]);
+
--- src/PSIOG.Workbench/app/scripts/directives/generator/generator.js
+++ src/PSIOG.Workbench/app/scripts/directives/generator/generator.js
@@ -11,28 +11,22 @@
 
 
 angular.module('sbAdminApp')
-    // .controller('testCaseCtrl', function($scope){
-    //     $scope.ddlValueChanged = function(){
-    //               console.log($scope.itemSelected);
-    //             };
-        
-    //  })
     .directive('generator', ['generatorService', function (generatorService) {       
         return {
             templateUrl: 'scripts/directives/generator/generator.html',
             //template : '<p>HELLO</p>',
-            scope: false,
             restrict: 'E',            
             link: function (scope) {
                 scope.xmlpaste = '';
                 scope.pastexml = true;
                 scope.jsonxmltoggle = true;
-                scope.toggleJsonXml = function() {
-                    //scope.jsonxmltoggle = !scope.jsonxmltoggle;
-                    alert('132');
+                scope.toggleJsonXml = function () {
+                    scope.jsonxmltoggle = !scope.jsonxmltoggle;
+                }
+                scope.ddlValueChanged = function(){
+                    console.log(scope.itemSelected);
                 }
                 scope.jsonfiles = ["1.json", "2.json"];
-                
                 var nodes = [];
                 var links = [];
                 var outp = [];
@@ -90,15 +84,43 @@ angular.module('sbAdminApp')
                     }
                 }
 
-                scope.clickedGenerateJSON = function () {
-                    alert("Hi");
+                scope.clickedGenerateJSON = function (flag) {
+
                     if (flag)
                     { scope.displayCombinations(outp, nodes, links); }
                     else {
                         scope.loader = true;
                         scope.pastexml = false;
                         //Pass on the JSON file name, pull in the JSON data, and send it to the generator Service
-                        generatorService.generateFromJson($scope.itemSelected)
+                        
+                        generatorService.getJsonValue(scope.itemSelected.flowChartID)
+                        .then(function(pass){
+                            
+                            var json = pass.data.Flowchart[0];
+                            var obj = { nodes: [], links: [] };
+
+                            var loop = 0;
+                            $.each(json.nodeDataArray, function () {
+                                var primary = 'N';
+                                if (loop == 0)
+                                    primary = 'P';
+                                obj.nodes.push({ id: this.key, text: this.text, primary: primary });
+                                loop++;
+                            });
+
+                            loop = 0;
+                            
+                            $.each(json.linkDataArray, function () {
+                                var myGuid = GUID();
+                                GUID.register(myGuid);
+                                obj.links.push({ from: this.from, to: this.to, text: "", id: myGuid }); //generateUniqueNumber(loop)
+                                loop++;
+                            })
+
+
+                            console.log(obj);
+
+                          generatorService.generateFromJson(obj)
                             .then(function (success) {
 
                                 scope.loader = false;
@@ -139,127 +161,145 @@ angular.module('sbAdminApp')
                                 scope.displayCombinations(outp, nodes, links);
                                 console.log(success);
                             }, function (failure) { console.log(failure); });
+
+
+
+                        }, function(fail){});
+                        
                     }
                 }
 
+                scope.goBack = function(){
+                    scope.pastexml = true;
+                    scope.results=false;
+                    nodes = [];
+                    links = [];
+                    //resultsData = [];
+                    outp = [];
+
+
+                }
+
                 scope.nodeList = function ()
-                        {   
+                {
 
-                                            semioutp = [];
-                                            //outp = resultsData;
-                                            console.log(outp);
-                                            
-
-                                            // var startNode = scope.startNode;
-                                            var startNodeValue = scope.startNode;
-
-                                            //var endNode = document.getElementById("endNode");
-                                            //var endNodeValue = endNode.options[endNode.selectedIndex].value;
-                                            console.log(startNodeValue);
-
-                                            //var endNode = document.getElementById("endNode");
-                                            var endNodeValue = scope.endNode;
-
-
-
-                                            if (startNodeValue == -1 && endNodeValue == -1)
-                                            { semioutp = outp;}
-                            if(startNodeValue!=-1 && endNodeValue==-1) //startNode's value and endNode's value doesnt exist
-                            {
-                            for(var i=0; i<outp.length; i++)
-                                {
-                            if(outp[i].slice(0, outp[i].indexOf("_"))==startNodeValue)
-                                semioutp.push(outp[i]);
-
-                            if(outp[i].indexOf("_"+startNodeValue+"_")!=-1)
-
-                            semioutp.push(outp[i].slice(outp[i].indexOf("_"+startNodeValue+"_")+1, outp[i].length));
-                                        console.log(semioutp);
-                                        
-                            
-                            }}
-
-                            if(endNodeValue!=-1 && startNodeValue==-1) //startNode's value doesnt and endNode's value exist
-                            {
-                            for(var i=0; i<outp.length; i++)
-                                {
-                            
-                            if(outp[i].slice(outp[i].lastIndexOf("_")+1, outp[i].length)==endNodeValue)
-                                semioutp.push(outp[i]);
-
-                            else if(outp[i].indexOf("_"+endNodeValue+"_")!=-1)
-
-                            semioutp.push(outp[i].slice(0, outp[i].indexOf("_"+endNodeValue+"_")+("_"+endNodeValue+"_").length));
-                                        console.log(semioutp);
-
-                                }
-                            }
-
-                            else //startNode's value and endNode's value exist
-                            {
-                            for(var i=0; i<outp.length; i++)
-                                {
-                            //Handling when case starts with given Start Node - when it ends in End Node or when it doesn't
-                            if(outp[i].slice(0, outp[i].indexOf("_"))==startNodeValue)
-                                {
-                                if(outp[i].slice(outp[i].lastIndexOf("_")+1, outp[i].length)==endNodeValue)
-                                    semioutp.push(outp[i]);
-
-                                else if(outp[i].indexOf("_"+endNodeValue+"_")!=-1)
-
-                                semioutp.push(outp[i].slice(0, outp[i].indexOf("_"+endNodeValue+"_")+("_"+endNodeValue+"_").length));
-                                            console.log(semioutp);
-                                
-                                }
-
-                            //Handling when startNode isnt at start
-                            else if(outp[i].indexOf("_"+startNodeValue+"_")!=-1)
-                            {
-
-                                if(outp[i].indexOf("_"+endNodeValue+"_")!=-1 || outp[i].slice(outp[i].lastIndexOf("_")+1, outp[i].length)==endNodeValue)
-
-                                {
-
-                                if(outp[i].slice(outp[i].lastIndexOf("_")+1, outp[i].length)==endNodeValue)
-                                {
-                                    semioutp.push(outp[i].slice(outp[i].indexOf("_"+startNodeValue+"_")+1, outp[i].length));
-                                            console.log(semioutp);
-                                }
-
-                                else if(outp[i].indexOf("_"+startNodeValue+"_") < outp[i].indexOf("_"+endNodeValue+"_"))
-                                {
-                                    semioutp.push(outp[i].slice(outp[i].indexOf("_"+startNodeValue+"_")+1, outp[i].indexOf("_"+endNodeValue+"_")+("_"+endNodeValue+"_").length));
-                                            console.log(semioutp);
-                                }
-
-                                }
-
-                            }
-
-                            
-                                        
-                            
-                            }
-                             }
-
-                        
-
-                            //Ensuring unique routes
-                            var uniqueRoutes = [];
-                            $.each(semioutp, function(i, el){
-                                if($.inArray(el, uniqueRoutes) === -1) uniqueRoutes.push(el);
-                            });
-
-                            console.log(semioutp);
-                            semioutp = uniqueRoutes;
-                            scope.displayCombinations(semioutp, nodes, links);
+                    semioutp = [];
+                    //outp = resultsData;
+                    console.log(outp);
                     
+
+                    // var startNode = scope.startNode;
+                    var startNodeValue = scope.startNode;
+
+                    //var endNode = document.getElementById("endNode");
+                    //var endNodeValue = endNode.options[endNode.selectedIndex].value;
+                    console.log(startNodeValue);
+
+                    //var endNode = document.getElementById("endNode");
+                    var endNodeValue = scope.endNode;
+
+
+
+                    if (startNodeValue == -1 && endNodeValue == -1)
+                    { semioutp = outp;}
+      if(startNodeValue!=-1 && endNodeValue==-1) //startNode's value and endNode's value doesnt exist
+      {
+       for(var i=0; i<outp.length; i++)
+          {
+      if(outp[i].slice(0, outp[i].indexOf("_"))==startNodeValue)
+        semioutp.push(outp[i]);
+
+      if(outp[i].indexOf("_"+startNodeValue+"_")!=-1)
+
+       semioutp.push(outp[i].slice(outp[i].indexOf("_"+startNodeValue+"_")+1, outp[i].length));
+                  console.log(semioutp);
+                  
+       
+    }}
+
+      if(endNodeValue!=-1 && startNodeValue==-1) //startNode's value doesnt and endNode's value exist
+      {
+       for(var i=0; i<outp.length; i++)
+          {
+     
+      if(outp[i].slice(outp[i].lastIndexOf("_")+1, outp[i].length)==endNodeValue)
+        semioutp.push(outp[i]);
+
+      else if(outp[i].indexOf("_"+endNodeValue+"_")!=-1)
+
+       semioutp.push(outp[i].slice(0, outp[i].indexOf("_"+endNodeValue+"_")+("_"+endNodeValue+"_").length));
+                  console.log(semioutp);
+
+          }
+      }
+
+      else //startNode's value and endNode's value exist
+      {
+       for(var i=0; i<outp.length; i++)
+          {
+      //Handling when case starts with given Start Node - when it ends in End Node or when it doesn't
+      if(outp[i].slice(0, outp[i].indexOf("_"))==startNodeValue)
+        {
+          if(outp[i].slice(outp[i].lastIndexOf("_")+1, outp[i].length)==endNodeValue)
+            semioutp.push(outp[i]);
+
+          else if(outp[i].indexOf("_"+endNodeValue+"_")!=-1)
+
+           semioutp.push(outp[i].slice(0, outp[i].indexOf("_"+endNodeValue+"_")+("_"+endNodeValue+"_").length));
+                      console.log(semioutp);
+          
+        }
+
+      //Handling when startNode isnt at start
+      else if(outp[i].indexOf("_"+startNodeValue+"_")!=-1)
+      {
+
+        if(outp[i].indexOf("_"+endNodeValue+"_")!=-1 || outp[i].slice(outp[i].lastIndexOf("_")+1, outp[i].length)==endNodeValue)
+
+        {
+
+          if(outp[i].slice(outp[i].lastIndexOf("_")+1, outp[i].length)==endNodeValue)
+           {
+            semioutp.push(outp[i].slice(outp[i].indexOf("_"+startNodeValue+"_")+1, outp[i].length));
+                       console.log(semioutp);
+           }
+
+           else if(outp[i].indexOf("_"+startNodeValue+"_") < outp[i].indexOf("_"+endNodeValue+"_"))
+           {
+            semioutp.push(outp[i].slice(outp[i].indexOf("_"+startNodeValue+"_")+1, outp[i].indexOf("_"+endNodeValue+"_")+("_"+endNodeValue+"_").length));
+                       console.log(semioutp);
+           }
+
+        }
+
+      }
+
+      
+                  
+       
+    }
+  }
+
+  
+
+    //Ensuring unique routes
+    var uniqueRoutes = [];
+    $.each(semioutp, function(i, el){
+        if($.inArray(el, uniqueRoutes) === -1) uniqueRoutes.push(el);
+    });
+
+    console.log(semioutp);
+    semioutp = uniqueRoutes;
+    scope.displayCombinations(semioutp, nodes, links);
+    
 
                 }
 
                 scope.displayCombinations = function(outputArray, nodes, links, first)
      {
-       
+       //Display combinatiomns
+    //console.log(outputArray);
+    //Displaying
      var dataTable = [];
      for(var i=0; i<outputArray.length; i++)
       {
